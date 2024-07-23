e5a3a599dc30afc9c56a412bb1c9685c72742f25
samimhub <aktarsamim529@gmail.com>
2024-07-23 15:36:53 +0530

    Commite

--- src/app/api/Tickets/route.js
+++ src/app/api/Tickets/route.js
@@ -1,31 +1,31 @@
 import Ticket from "@/app/models/Ticket";
 import { NextResponse } from "next/server";
-import { dbConnect } from  "@/app/lib/dbConnect";
+import { dbConnect } from "@/app/lib/dbConnect";
 
-export async function GET(req){
-    await dbConnect();
-    
-    try{
-        const tickets =await Ticket.find();
-        return NextResponse.json({tickets},{status:200})
-    }catch (err) {
-      console.log(err);
-      return NextResponse.json({ message: "Error", err }, { status: 500 });
-    }
+export async function GET(req) {
+  await dbConnect();
+
+  try {
+    const tickets = await Ticket.find();
+    return NextResponse.json({ tickets }, { status: 200 })
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  }
 }
 
 
 export async function POST(req) {
-    await dbConnect();
+  await dbConnect();
 
-    try {
-      const body = await req.json();
-      const {formData}=body;
-      await Ticket.create(formData);
-  
-      return NextResponse.json({ message: "Ticket Created" }, { status: 201 });
-    } catch (err) {
-      console.log(err);
-      return NextResponse.json({ message: "Error", err }, { status: 500 });
-    }
+  try {
+    const body = await req.json();
+    const { formData } = body;
+    await Ticket.create(formData);
+
+    return NextResponse.json({ message: "Ticket Created" }, { status: 201 });
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
   }
+}
--- src/app/middleware.js
+++ src/app/middleware.js
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-const allowedOrigins = ['https://your-domain.com', 'http://localhost:3000'];
+const allowedOrigins = [ 'http://localhost:3000'];
 
 const corsOptions = {
   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
