f84dfc5169991c0492c31b3d5061ea58b776a870
samimhub <aktarsamim529@gmail.com>
2024-07-23 19:22:23 +0530

    Commite

--- src/app/middleware.js
+++ src/app/middleware.js
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-const allowedOrigins = [ 'http://localhost:3000'];
+const allowedOrigins = [ 'https://quickticket-creator-app.vercel.app/'];
 
 const corsOptions = {
   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
